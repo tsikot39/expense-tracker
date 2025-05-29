9abb6b9da58ce7a8e6bf6032ef109ce8b92153b8
tsikot39 <corpusjohnbenedict@gmail.com>
2025-05-29 13:47:37 -0600

    add ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "100MB",
+        },
+    },
 };
 
 export default nextConfig;
